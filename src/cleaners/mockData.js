fa334a7bd7151e908d335a519bea77236f760dac
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-05-15 16:44:19 -0600

    Add fetch test

---
+++ src/cleaners/mockData.js
@@ -0,0 +1,62 @@
+export const mockRawData = {
+  "results": [
+    {
+      "vote_count": 3383,
+      "id": 299536,
+      "video": false,
+      "vote_average": 8.6,
+      "title": "Avengers: Infinity War",
+      "popularity": 555.264536,
+      "poster_path": "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+      "original_language": "en",
+      "original_title": "Avengers: Infinity War",
+      "genre_ids": [
+        12,
+        878,
+        14,
+        28
+      ],
+      "backdrop_path": "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+      "adult": false,
+      "overview": "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
+      "release_date": "2018-04-25"
+    },
+    {
+      "vote_count": 513,
+      "id": 427641,
+      "video": false,
+      "vote_average": 5.9,
+      "title": "Rampage",
+      "popularity": 157.877408,
+      "poster_path": "/30oXQKwibh0uANGMs0Sytw3uN22.jpg",
+      "original_language": "en",
+      "original_title": "Rampage",
+      "genre_ids": [
+        28,
+        12,
+        878
+      ],
+      "backdrop_path": "/wrqUiMXttHE4UBFMhLHlN601MZh.jpg",
+      "adult": false,
+      "overview": "Primatologist Davis Okoye shares an unshakable bond with George, the extraordinarily intelligent, silverback gorilla who has been in his care since birth.  But a rogue genetic experiment gone awry mutates this gentle ape into a raging creature of enormous size.  To make matters worse, it’s soon discovered there are other similarly altered animals.  As these newly created alpha predators tear across North America, destroying everything in their path, Okoye teams with a discredited genetic engineer to secure an antidote, fighting his way through an ever-changing battlefield, not only to halt a global catastrophe but to save the fearsome creature that was once his friend.",
+      "release_date": "2018-04-12"
+    }
+  ]
+};
+
+export const mockCleanData = [
+  {
+    "id": 299536,
+    "title": "Avengers: Infinity War",
+    "poster": "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    "backdrop": "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+    "overview": "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
+  },
+  {
+    "id": 427641,
+    "title": "Rampage",
+    "poster": "/30oXQKwibh0uANGMs0Sytw3uN22.jpg",
+    "backdrop": "/wrqUiMXttHE4UBFMhLHlN601MZh.jpg",
+    "overview": "Primatologist Davis Okoye shares an unshakable bond with George, the extraordinarily intelligent, silverback gorilla who has been in his care since birth.  But a rogue genetic experiment gone awry mutates this gentle ape into a raging creature of enormous size.  To make matters worse, it’s soon discovered there are other similarly altered animals.  As these newly created alpha predators tear across North America, destroying everything in their path, Okoye teams with a discredited genetic engineer to secure an antidote, fighting his way through an ever-changing battlefield, not only to halt a global catastrophe but to save the fearsome creature that was once his friend."
+  }
+];
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,16 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
+import { mockRawData, mockCleanData } from '../../cleaners/mockData';
 
 describe('App', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(<App />)
+    wrapper = shallow(<App />, { disableLifecycleMethods: true })
   });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('getMovies', () => {
+    let wrapper;
+    
+    beforeEach(() => {
+      wrapper = shallow(<App />, { disableLifecycleMethods: true })
+    });
+    it('should call fetch on the movie api', async () => {
+      //setup
+      window.fetch = jest.fn().mockImplementation(()=>Promise.resolve({
+        status: 200,
+        json: ()=>Promise.resolve(mockRawData)
+      })
+    )
+      //execution
+      await wrapper.instance().getMovies()
+      //expectation
+      expect(window.fetch).toHaveBeenCalled();
+    });
+
+    it('should return an error object if the response is a rejection', () => {
+      
+    });
+  });
+  
 
 
 });
