625e368e8a64b59055f867deabf7b12173b4c357
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-05-15 16:53:54 -0600

    Add rejection tests to getMovies describe block in app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -34,7 +34,40 @@ describe('App', () => {
     });
 
     it('should return an error object if the response is a rejection', () => {
-      
+      //setup
+      window.fetch = jest.fn().mockImplementation(() => Promise.reject(new Error('test rejection'))
+      )
+      //execution
+      await wrapper.instance().getMovies()
+      //expectation
+      const expected = [{
+        id: '00',
+        title: 'we are sorry!',
+        poster: '',
+        backdrop: '',
+        overview: 'Sorry!  We encountered an error and were unable to retrieve your data.'
+      }]
+      expect().toEqual(expected)
+    });
+
+    it('should return an error object if the response status is not 200', () => {
+      //setup
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        status: 408,
+        json: () => Promise.resolve(mockRawData)
+      })
+      )
+      //execution
+      await wrapper.instance().getMovies()
+      //expectation
+      const expected = [{
+        id: '00',
+        title: 'we are sorry!',
+        poster: '',
+        backdrop: '',
+        overview: 'Sorry!  We encountered an error and were unable to retrieve your data.'
+      }]
+      expect().toEqual(expected)
     });
   });
   
