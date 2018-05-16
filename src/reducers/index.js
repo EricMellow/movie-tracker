0c334f74bf7849e836ebd37376ba07342e194a20
Tyler McNamara <mcnamara14@gmail.com>
2018-05-16 17:13:13 -0600

    Add wireframe images

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import { recentMoviesReducer } from './recentMoviesReducer';
-import { addRecentMovies } from '../actions/index';
 
 export const rootReducer = combineReducers({
   recentMovies: recentMoviesReducer
