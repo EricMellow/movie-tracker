51be920094545dc8f4c0cb9b1b9467674a0b7323
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-05-19 15:15:19 -0600

    Add selected movie as background image

--- src/components/FeaturedMovie/FeaturedMovie.js
+++ src/components/FeaturedMovie/FeaturedMovie.js
@@ -3,25 +3,35 @@ import './FeaturedMovie.css';
 import { connect } from 'react-redux';
 
 class FeaturedMovie extends Component {
-  
+
+  componentDidMount() {
+
+  }
+
+
   render() {
-    const featuredMovie = this.props.recentMovies.find(movie=>{
-      
-      return movie.id === this.props.movieId
-    })
-    console.log(featuredMovie)
-    return (
-      <div className="featuredMovie">
-        FEATURED MOVIE
-      </div>
-    )
+    const featuredMovie = this.props.recentMovies.find(movie => {
+      return movie.id === this.props.movieId;
+    });
+    if (featuredMovie) {
+      const image = `https://image.tmdb.org/t/p/w1280${featuredMovie.backdrop}`;
+      const background = {backgroundImage: `url( ${image} )`}
+      return (
+        <div className="featuredMovie" style={background} > 
+        </div>
+      );
+    } else {
+      return (
+        <p>LOADING</p>);
+    }
+
   }
 }
 
-const mapStateToProps = (state)=>({
+const mapStateToProps = (state) => ({
   recentMovies: state.recentMovies,
   movieId: state.selectedMovieId
-})
+});
 
 export default connect(mapStateToProps)(FeaturedMovie);
 
