019a09682d763a7b82c731c5394b744e3a8ec0b7
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-05-19 15:37:26 -0600

    Add selected movie style

--- src/components/FeaturedMovie/FeaturedMovie.js
+++ src/components/FeaturedMovie/FeaturedMovie.js
@@ -15,9 +15,17 @@ class FeaturedMovie extends Component {
     });
     if (featuredMovie) {
       const image = `https://image.tmdb.org/t/p/w1280${featuredMovie.backdrop}`;
-      const background = {backgroundImage: `url( ${image} )`}
+      const background = { backgroundImage: `url( ${image} )` }
       return (
-        <div className="featuredMovie" style={background} > 
+        <div className="featuredMovie" style={background} >
+          <div className="favoriteButton">
+            <p>Add to Favorites</p>
+          </div>
+          <div className="movieOverview">
+            <h2>{featuredMovie.title}</h2>
+            <p>{featuredMovie.overview} ...</p>
+            
+          </div>
         </div>
       );
     } else {
