d1f49b48208c6911a58c5b231a93b7a16c1d5609
Tyler McNamara <mcnamara14@gmail.com>
2018-05-21 21:20:36 -0600

    Add default state and handleLoginLogoutClick tests

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Header } from '../Header/Header';
 import { shallow } from 'enzyme';
+import { createMemoryHistory } from 'history'
 
 describe('Header', () => {
   let wrapper;
@@ -13,5 +14,39 @@ describe('Header', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('should have default state', ()=> {
+    const expected = {
+      favoritesError: false
+    }
+
+    expect(wrapper.state()).toEqual(expected);
+  })
+
+  describe('handleLoginLogoutClick', () => {
+    beforeEach(() => {
+      const mockLogout = jest.fn();
+      const history = createMemoryHistory('/')
+      const mockUserId = 3;
+
+      wrapper = shallow(<Header userId={mockUserId} logout={mockLogout} history={history} />);
+    });
+
+    it('should call logout if user is signed in', () => {
+      const result = wrapper.instance().props.logout;
+      wrapper.instance().setRecentFeaturedMovie = jest.fn();
+      wrapper.instance().handleLoginLogoutClick();
+
+      expect(result).toHaveBeenCalled();
+    });
+
+    it('should call setRecentFeaturedMovie if user is signed in', () => {
+      // const result = wrapper.instance().setRecentFeaturedMovie;
+      wrapper.instance().setRecentFeaturedMovie = jest.fn();
+      wrapper.instance().handleLoginLogoutClick();
+
+      expect(wrapper.instance().setRecentFeaturedMovie).toHaveBeenCalled();
+    });
   });
+
+});
   
