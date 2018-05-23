8354b4919b1d4f0f5a8b028e47d0e7bbf204f0fe
Tyler McNamara <mcnamara14@gmail.com>
2018-05-22 18:48:18 -0600

    Add userIdReducer tests

---
+++ src/reducers/tests/userIdReducer.test.js
@@ -0,0 +1,41 @@
+import { userIdReducer } from "../userIdReducer";
+
+describe('userIdReducer', () => {
+
+  it('should add a user ID if an action type of SET_USER_ID is dispatched', () => {
+    const initialState = null;
+    const mockAction = {
+      type: 'SET_USER_ID',
+      userId: 12345
+    };
+
+    const result = userIdReducer(initialState, mockAction);
+    
+    const expected = 12345;
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return initial state if it is passed an action that has a type of LOGOUT', () => {
+    const initialState = null;
+    const mockAction = {
+      type: 'LOGOUT'
+    };
+
+    const result = userIdReducer(initialState, mockAction);
+
+    expect(result).toEqual(initialState);
+  });
+
+  it('should return state if it is passed an action that does not have a type of ADD_RECENT_MOVIES', () => {
+    const initialState = null;
+    const mockAction = {
+      type: 'YTHOU',
+      recentMovieData: [{title: 'Space Jam'}]
+    };
+
+    const result = userIdReducer(initialState, mockAction);
+
+    expect(result).toEqual(initialState);
+  });
+});
