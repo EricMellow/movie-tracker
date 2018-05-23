fd1e8913975667c88ffb590f05e61aba5d972942
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-05-22 18:37:51 -0600

    Add it blocks for featured Movie component tests

--- src/components/FeaturedMovie/FeaturedMovie.test.js
+++ src/components/FeaturedMovie/FeaturedMovie.test.js
@@ -29,12 +29,111 @@ describe('Card', () => {
       ]
     };
 
-    wrapper = shallow(<FeaturedMovie {...props}/>);
+    wrapper = shallow(<FeaturedMovie {...props} />);
   });
 
   it('should match the snapshot', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should instatiate with default state', () => {
+
+  })
+  describe('handleFavoriteClick', () => {
+    it('should call toggleStoreFavorites with the correct argument', () => {
+
+    });
+
+    it('should call toggleFeatureMove', () => {
+
+    });
+
+    it('should call togglePromptLogin', () => {
+
+    });
+  });
+
+  describe('toggleStoreFavorite', () => {
+    it('should call deleteFavorite with the correct argument if isAFavorite is true', () => {
+
+    });
+
+    it('should call addFavorite with the correct argument if isAFavorite is false', () => {
+
+    });
+  });
+
+  describe('toggleFeaturedMovie', () => {
+    it('should call setFeaturedMovie with the correct argument if on the favorites page', () => {
+
+    });
+
+    it('should not call setFeaturedMovie if not on the favorites page', () => {
+
+    });
+  });
+
+  describe('togglePromptLogin', () => {
+    it('should set the state promptLogin:true if the userId does not exist', () => {
+
+    });
+
+    it('should set the state with promptLogin: false within a setTimeout call ', () => {
+
+    });
+  });
+
+  describe('findFavorite', () => {
+    it('should return a favorite movie if it is found in the favoriteMovies in props', () => {
+
+    });
+  });
+
+  describe('deleteFavorite', () => {
+    it('should call deleteFavoriteovie with the correct argument', () => {
+
+    });
+
+    it('should call deleteFavoriteFromDatabase with the correct argument', () => {
+
+    });
+  });
+
+  describe('addFavorite', () => {
+    it('should call addFavoriteMovie with the correct argument', () => {
+
+    });
+
+    it('should call addFavoriteToDatabase with the correct argument', () => {
+
+    });
+  });
+
+  describe('addFavoriteToDatabase', () => {
+    it('should call fetch with the correct arguments', () => {
+
+    });
+  });
+
+  describe('deleteFavoriteFromDatabase', () => {
+    it('should call fetch with the correct arguments', () => {
+
+    });
+  });
+
+  describe('mapStateToProps', () => {
+    it('should map recentMovies, movieId, userId, favoriteMovies to props', () => {
+
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should map addFavoriteMovie, deleteFavoriteMovie, setFeaturedMove to props',
+      () => {
+
+      });
+  });
+
 
 });
